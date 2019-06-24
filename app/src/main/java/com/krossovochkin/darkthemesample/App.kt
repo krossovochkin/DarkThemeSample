769c5659fbe8f3a511e6ee2c934e59241ebb4ee8
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2019-06-24 22:07:26 +0300

    Navigation setup

---
+++ app/src/main/java/com/krossovochkin/darkthemesample/App.kt
@@ -0,0 +1,6 @@
+package com.krossovochkin.darkthemesample
+
+import android.app.Application
+
+class App : Application() {
+}
--- app/src/main/java/com/krossovochkin/darkthemesample/MainActivity.kt
+++ app/src/main/java/com/krossovochkin/darkthemesample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.krossovochkin.darkthemesample
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.buttonDefault).setOnClickListener {
+            startActivity(Intent(this, DefaultActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.buttonDayNight).setOnClickListener {
+            startActivity(Intent(this, DayNightActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.buttonForceDark).setOnClickListener {
+            startActivity(Intent(this, ForceDarkActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.buttonNoDark).setOnClickListener {
+            startActivity(Intent(this, NoDarkActivity::class.java))
+        }
     }
 }
