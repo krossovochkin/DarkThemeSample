ea60a0e4ebeec657a03cfcb046636d2a2b6d5361
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2019-06-26 21:58:37 +0300

    Code clean-up Version-aware menu items Version-aware default option

--- app/src/main/java/com/krossovochkin/darkthemesample/theme/ThemeUtils.kt
+++ app/src/main/java/com/krossovochkin/darkthemesample/theme/ThemeUtils.kt
@@ -1,16 +1,26 @@
 package com.krossovochkin.darkthemesample.theme
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatDelegate.*
 
 @NightMode
 fun resolveTheme(key: String?): Int = when (key) {
-    "light" -> MODE_NIGHT_NO
-    "dark" -> MODE_NIGHT_YES
-    "dark_battery_save" -> MODE_NIGHT_AUTO_BATTERY
-    "system_default" -> MODE_NIGHT_FOLLOW_SYSTEM
+    LIGHT -> MODE_NIGHT_NO
+    DARK -> MODE_NIGHT_YES
+    DARK_BATTERY_SAVE -> MODE_NIGHT_AUTO_BATTERY
+    SYSTEM_DEFAULT -> MODE_NIGHT_FOLLOW_SYSTEM
     else -> throw IllegalArgumentException("Unknown theme key: $key")
 }
 
 fun getDefaultThemeKey(): String {
-    return "system_default"
+    return if (Build.VERSION.SDK_INT >= 28) {
+        SYSTEM_DEFAULT
+    } else {
+        DARK_BATTERY_SAVE
+    }
 }
+
+private const val LIGHT = "light"
+private const val DARK = "dark"
+private const val DARK_BATTERY_SAVE = "dark_battery_save"
+private const val SYSTEM_DEFAULT = "system_default"
